a4ee3a0a4908a100b7b17bfdf43bd716656bdaef
faker683 <yhr1201@126.com>
2026-04-07 12:52:57 +0800

    第一次更新测试

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 
 export const siteConfig: SiteConfig = {
 	title: "踏实的强酸柠檬の家",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	subtitle: "On top of the world",
+	lang: "zh_CN", // Language code, e.g. 'en', 'en', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
